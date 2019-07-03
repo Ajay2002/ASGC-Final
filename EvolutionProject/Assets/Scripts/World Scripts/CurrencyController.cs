5630795a9443859304cce4e1ce8fc0d24706f493
Tristan Burgess <paradox.m.burgess@gmail.com>
2019-07-03 15:07:57 +1000

    CurrencyController
    currencycontroller created that other scripts can call add/remove currency with, and the function will return whether or not it needed to clamp the end result to within 0 and its max bound - also option to not remove currency if there isn't enough there.

---
+++ EvolutionProject/Assets/Scripts/World Scripts/CurrencyController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencyController : MonoBehaviour
+{
+    public static CurrencyController Instance;
+
+    [SerializeField, Min(0)]
+    private int currencyAmountUpperBound;
+
+    [SerializeField]
+    private int currentCurrencyAmount;
+
+    private void Start()
+    {
+        if (Instance == null) Instance = this;
+        
+    }
+
+    private void OnValidate ()
+    {
+        if (currencyAmountUpperBound < 0) currencyAmountUpperBound = 0;
+        
+    }
+
+    public bool AddCurrency (int amount) //
+    {
+        bool unclamped = !(currentCurrencyAmount + Mathf.Abs(amount) > currencyAmountUpperBound);
+
+        currentCurrencyAmount += Mathf.Abs(amount);
+
+        currentCurrencyAmount = Mathf.Clamp(currentCurrencyAmount, 0, currencyAmountUpperBound);
+
+        return unclamped;
+    }
+
+    public bool RemoveCurrency (int amount, bool cancelOnInsufficientCurrency)
+    {
+        bool sufficientCurrency = !(currentCurrencyAmount < Mathf.Abs(amount));
+        if (cancelOnInsufficientCurrency && !sufficientCurrency) return false;
+        
+        currentCurrencyAmount -= Mathf.Abs(amount);
+        
+        currentCurrencyAmount = Mathf.Clamp(currentCurrencyAmount, 0, currencyAmountUpperBound);
+
+        return sufficientCurrency;
+    }
+}
