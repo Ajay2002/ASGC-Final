a74eef3c51fc0584db90f830a2bc1365ea66a2d0
Tristan Burgess <paradox.m.burgess@gmail.com>
2019-07-03 17:00:16 +1000

    Food Spawning
    ScriptableObjects used to define the parameters of the food and the food spawners.

---
+++ EvolutionProject/Assets/ScriptableObjects/FoodSpawnerScriptableObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Food Spawner", menuName = "Food/Food Spawner")]
+public class FoodSpawnerScriptableObject : ScriptableObject
+{
+    public String type;
+    public FoodScriptableObject food;
+    public GameObject prefab;
+    public float spawnPeriod;
+    public float spawnDistance;
+}
