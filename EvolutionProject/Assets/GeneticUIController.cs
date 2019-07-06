9034a283a020475ce1bdaa981508fc9b4caee454
Ajay Venkat <ajays.workemail@gmail.com>
2019-07-06 15:40:57 +1000

    Side Scene

--- EvolutionProject/Assets/GeneticUIController.cs
+++ EvolutionProject/Assets/GeneticUIController.cs
@@ -6,17 +6,10 @@ using UnityEngine.UI;
 public class GeneticUIController : MonoBehaviour
 {
 
-    public Canvas dynamicCanvas;
-
-    public Transform selectedEntity;
 
     private void Update() {
         
-        Vector3 position = selectedEntity.position + Vector3.up*6;
-        dynamicCanvas.transform.position = position;
-
-        dynamicCanvas.transform.LookAt(Camera.main.transform.position);
-
+     
     }
 
 }
