d3ec71fde8843590ee689bcb91b96c803c7a38e6
Ajay Venkat <ajays.workemail@gmail.com>
2019-07-06 13:52:40 +1000

    E

---
+++ EvolutionProject/Assets/GeneticUIController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeneticUIController : MonoBehaviour
+{
+
+    public Canvas dynamicCanvas;
+
+    public Transform selectedEntity;
+
+    private void Update() {
+        
+        Vector3 position = selectedEntity.position + Vector3.up*6;
+        dynamicCanvas.transform.position = position;
+
+        dynamicCanvas.transform.LookAt(Camera.main.transform.position);
+
+    }
+
+}
